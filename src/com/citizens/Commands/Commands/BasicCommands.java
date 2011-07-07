4ff70e21b23a47da576988120f9e5012b70dd69b
Desttro <petr@sabrnak.cz>
2011-07-07 16:24:32 +0200

    Translated BasicCommands.java

--- src/com/citizens/Commands/Commands/BasicCommands.java
+++ src/com/citizens/Commands/Commands/BasicCommands.java
@@ -38,7 +38,7 @@ public class BasicCommands {
 	@Command(
 			aliases = "citizens",
 			usage = "",
-			desc = "view Citizens info",
+			desc = "zobrazi Citizens info",
 			modifiers = "",
 			min = 0,
 			max = 0)
@@ -49,17 +49,17 @@ public class BasicCommands {
 			HumanNPC npc) {
 		sender.sendMessage(ChatColor.GREEN
 				+ StringUtils.listify(StringUtils.wrap("Citizens")));
-		sender.sendMessage(ChatColor.GREEN + "  Version: "
+		sender.sendMessage(ChatColor.GREEN + "  Verze: "
 				+ StringUtils.wrap(Citizens.getVersion()));
-		sender.sendMessage(ChatColor.GREEN + "  Authors: ");
+		sender.sendMessage(ChatColor.GREEN + "  Autori: ");
 		sender.sendMessage(ChatColor.YELLOW + "      - fullwall");
 		sender.sendMessage(ChatColor.YELLOW + "      - aPunch");
 	}
 
 	@Command(
 			aliases = "citizens",
-			usage = "help (page)",
-			desc = "view the Citizens help page",
+			usage = "help (strana)",
+			desc = "zobrazi pomocnou stranku Citizens",
 			modifiers = "help",
 			min = 1,
 			max = 2)
@@ -79,7 +79,7 @@ public class BasicCommands {
 	@Command(
 			aliases = "citizens",
 			usage = "reload",
-			desc = "reload Citizens",
+			desc = "obnovi Citizens",
 			modifiers = "reload",
 			min = 1,
 			max = 1)
@@ -87,24 +87,24 @@ public class BasicCommands {
 	@ServerCommand()
 	public static void reload(CommandContext args, CommandSender sender,
 			HumanNPC npc) {
-		Messaging.log("Reloading configuration settings....");
+		Messaging.log("Obnovuji konfiguraci....");
 		sender.sendMessage(ChatColor.GREEN + "[" + StringUtils.wrap("Citizens")
-				+ "] Reloading....");
+				+ "] Obnovuji....");
 
 		UtilityProperties.initialize();
 		PropertyManager.loadAll();
 		Constants.setupVariables();
 
-		Messaging.log("Reloaded.");
+		Messaging.log("Obnoveno.");
 		sender.sendMessage(ChatColor.GREEN + "[" + StringUtils.wrap("Citizens")
-				+ "] Reloaded.");
+				+ "] Obnoveno.");
 	}
 
 	@CommandRequirements()
 	@Command(
 			aliases = "citizens",
 			usage = "save",
-			desc = "force a save of Citizens files",
+			desc = "ulozi Citizens soubory",
 			modifiers = "save",
 			min = 1,
 			max = 1)
@@ -113,23 +113,23 @@ public class BasicCommands {
 	public static void forceSave(CommandContext args, CommandSender sender,
 			HumanNPC npc) {
 		if (sender instanceof Player)
-			Messaging.log("Saving...");
+			Messaging.log("Ukladani...");
 		sender.sendMessage(ChatColor.GREEN + "[" + StringUtils.wrap("Citizens")
-				+ "] Saving...");
+				+ "] Ukladani...");
 
 		PropertyManager.saveState();
 
 		if (sender instanceof Player)
-			Messaging.log("Saved.");
+			Messaging.log("Ulozeno.");
 		sender.sendMessage(ChatColor.GREEN + "[" + StringUtils.wrap("Citizens")
-				+ "] Saved.");
+				+ "] Ulozeno.");
 	}
 
 	@CommandRequirements()
 	@Command(
 			aliases = { "basic", "npc" },
-			usage = "help (page)",
-			desc = "view the Basic NPC help page",
+			usage = "help (strana)",
+			desc = "zobrazi (zakladni) Basic NPC pomocnou stranu",
 			modifiers = "help",
 			min = 1,
 			max = 2)
@@ -147,8 +147,8 @@ public class BasicCommands {
 	@CommandRequirements()
 	@Command(
 			aliases = "npc",
-			usage = "create [name] (text)",
-			desc = "create an NPC",
+			usage = "create [jmeno] (text)",
+			desc = "vytvori NPC",
 			modifiers = "create",
 			min = 2)
 	@CommandPermissions("create.basic")
@@ -166,7 +166,7 @@ public class BasicCommands {
 		}
 		if (firstArg.length() > 16) {
 			player.sendMessage(ChatColor.RED
-					+ "The name of this NPC will be truncated - max name length is 16.");
+					+ "Jmeno tohoto NPC bude zkaceno - maximalni delka je 16.");
 			firstArg = args.getString(1).substring(0, 16);
 		}
 		int UID = NPCManager.register(firstArg, player.getLocation(),
@@ -194,14 +194,14 @@ public class BasicCommands {
 	@Command(
 			aliases = "npc",
 			usage = "move",
-			desc = "move an NPC",
+			desc = "premisti NPC",
 			modifiers = "move",
 			min = 1,
 			max = 1)
 	@CommandPermissions("modify.basic")
 	public static void moveNPC(CommandContext args, Player player, HumanNPC npc) {
 		player.sendMessage(StringUtils.wrap(npc.getStrippedName())
-				+ " is enroute to your location!");
+				+ " je na ceste do vasi lokace!");
 		npc.teleport(player.getLocation());
 		npc.getNPCData().setLocation(player.getLocation());
 	}
@@ -209,7 +209,7 @@ public class BasicCommands {
 	@Command(
 			aliases = "npc",
 			usage = "moveTo [x y z](world pitch yaw)",
-			desc = "move an NPC to a location",
+			desc = "premisti NPC do lokace",
 			modifiers = "moveto",
 			min = 4,
 			max = 4)
@@ -251,7 +251,7 @@ public class BasicCommands {
 	@Command(
 			aliases = "npc",
 			usage = "copy",
-			desc = "copy an NPC",
+			desc = "zkopiruje NPC",
 			modifiers = "copy",
 			min = 1,
 			max = 1)
@@ -275,7 +275,7 @@ public class BasicCommands {
 	@Command(
 			aliases = "npc",
 			usage = "remove (all)",
-			desc = "remove NPCs",
+			desc = "smaze NPC(cka)",
 			modifiers = "remove",
 			min = 1,
 			max = 2)
@@ -286,7 +286,7 @@ public class BasicCommands {
 			if (Permission.isAdmin(player)) {
 				NPCManager.removeAll();
 				NPCManager.deselectNPC(player);
-				player.sendMessage(ChatColor.GRAY + "The NPC(s) disappeared.");
+				player.sendMessage(ChatColor.GRAY + "NPC(cka) zmizel(a).");
 			} else {
 				player.sendMessage(MessageUtils.noPermissionsMessage);
 			}
@@ -295,13 +295,13 @@ public class BasicCommands {
 		NPCManager.remove(npc.getUID());
 		NPCManager.deselectNPC(player);
 		player.sendMessage(StringUtils.wrap(npc.getName(), ChatColor.GRAY)
-				+ " disappeared.");
+				+ " zmizel(a).");
 	}
 
 	@Command(
 			aliases = "npc",
-			usage = "rename [name]",
-			desc = "rename an NPC",
+			usage = "rename [jmeno]",
+			desc = "prejmenuje NPC",
 			modifiers = "rename",
 			min = 2,
 			max = 2)
@@ -311,18 +311,18 @@ public class BasicCommands {
 		String name = args.getString(1);
 		if (name.length() > 16) {
 			player.sendMessage(ChatColor.RED
-					+ "Max name length is 16 - NPC name length will be truncated.");
+					+ "Maximalni delka jmena je 16 - jmeno NPC bude zkraceno.");
 			name = name.substring(0, 16);
 		}
 		NPCManager.rename(npc.getUID(), name, npc.getOwner());
 		player.sendMessage(ChatColor.GREEN + StringUtils.wrap(npc.getName())
-				+ "'s name was set to " + StringUtils.wrap(name) + ".");
+				+ "' jmneno nastaveno na " + StringUtils.wrap(name) + ".");
 	}
 
 	@Command(
 			aliases = "npc",
-			usage = "color [color-code]",
-			desc = "set the name color of an NPC",
+			usage = "color [kod-barvy]",
+			desc = "nastavi barvu jmena NPC",
 			modifiers = "color",
 			min = 2,
 			max = 2)
@@ -330,10 +330,10 @@ public class BasicCommands {
 	public static void setNPCColour(CommandContext args, Player player,
 			HumanNPC npc) {
 		if (!args.getString(1).substring(0, 1).equals("&")) {
-			player.sendMessage(ChatColor.RED + "Use an & to specify color.");
+			player.sendMessage(ChatColor.RED + "Pouzij & k specifikaci barvy.");
 		} else if (args.getString(1).length() != 2) {
 			player.sendMessage(ChatColor.GRAY
-					+ "Use the format &(code). Example - &f = white.");
+					+ "Pouzij format &(kod). Priklad - &f = bila.");
 		} else {
 			int colour = 0xf;
 			try {
@@ -343,22 +343,22 @@ public class BasicCommands {
 					colour = Integer.parseInt(
 							args.getString(1).substring(1, 2), 16);
 				} catch (NumberFormatException e) {
-					player.sendMessage(ChatColor.RED + "Invalid colour code.");
+					player.sendMessage(ChatColor.RED + "Spatny kod barvy.");
 					return;
 				}
 			}
 			npc.getNPCData().setColour(colour);
 			NPCManager.setColour(npc.getUID(), npc.getOwner());
 			player.sendMessage(StringUtils.wrapFull("{" + npc.getStrippedName()
-					+ "}'s name color is now "
-					+ args.getString(1).replace("&", "\u00A7") + "this}."));
+					+ "}'s barva jmena je ted "
+					+ args.getString(1).replace("&", "\u00A7") + "tato}."));
 		}
 	}
 
 	@Command(
 			aliases = "npc",
 			usage = "set [text]",
-			desc = "set the text of an NPC",
+			desc = "nastavi text pro NPC",
 			modifiers = "set",
 			min = 2)
 	@CommandPermissions("modify.basic")
@@ -369,13 +369,13 @@ public class BasicCommands {
 		texts.add(text);
 		NPCManager.setText(npc.getUID(), texts);
 		player.sendMessage(StringUtils.wrapFull("{" + npc.getName()
-				+ "}'s text was set to {" + text + "}."));
+				+ "}'s text byl zmenen na {" + text + "}."));
 	}
 
 	@Command(
 			aliases = "npc",
 			usage = "add [text]",
-			desc = "add text to an NPC",
+			desc = "prida text pro NPC",
 			modifiers = "add",
 			min = 2)
 	@CommandPermissions("modify.basic")
@@ -383,14 +383,14 @@ public class BasicCommands {
 			HumanNPC npc) {
 		String text = args.getJoinedStrings(1);
 		NPCManager.addText(npc.getUID(), text);
-		player.sendMessage(StringUtils.wrap(text) + " was added to "
+		player.sendMessage(StringUtils.wrap(text) + " byl pridan pro "
 				+ StringUtils.wrap(npc.getStrippedName() + "'s") + " text.");
 	}
 
 	@Command(
 			aliases = "npc",
 			usage = "reset",
-			desc = "reset the text of an NPC",
+			desc = "resetuje text NPC",
 			modifiers = "reset",
 			min = 1,
 			max = 1)
@@ -399,13 +399,13 @@ public class BasicCommands {
 			HumanNPC npc) {
 		NPCManager.resetText(npc.getUID());
 		player.sendMessage(StringUtils.wrap(npc.getStrippedName() + "'s")
-				+ " text was reset!");
+				+ " text byl resetovan!");
 	}
 
 	@Command(
 			aliases = "npc",
 			usage = "item [item]",
-			desc = "set the item in an NPC's hand",
+			desc = "nastavi predmet v NPC's ruce",
 			modifiers = "item",
 			min = 2,
 			max = 2)
@@ -418,7 +418,7 @@ public class BasicCommands {
 	@Command(
 			aliases = "npc",
 			usage = "armor [armor] [item]",
-			desc = "set the armor of an NPC",
+			desc = "nastavi brneni pro NPC",
 			modifiers = "armor",
 			min = 3,
 			max = 3)
@@ -427,18 +427,18 @@ public class BasicCommands {
 			HumanNPC npc) {
 		Material mat = StringUtils.parseMaterial(args.getString(2));
 		if (mat == null) {
-			player.sendMessage(ChatColor.RED + "Invalid item.");
+			player.sendMessage(ChatColor.RED + "Neplatny predmet.");
 			return;
 		}
 		if (mat != Material.AIR && !player.getInventory().contains(mat)) {
 			player.sendMessage(ChatColor.RED
-					+ "You need to have the item in your inventory to add it to the NPC.");
+					+ "Musis mit predmet v inventari pokud jej chces dat NPC.");
 			return;
 		}
 		if ((mat.getId() < 298 || mat.getId() > 317)
 				&& (mat.getId() != 86 && mat.getId() != 91)) {
 			player.sendMessage(ChatColor.GRAY
-					+ "That can't be used as an armor material.");
+					+ "Tohle nemuze byt pouzito jako brneni.");
 			return;
 		}
 		int slot = player.getInventory().first(mat);
@@ -464,7 +464,7 @@ public class BasicCommands {
 		NPCManager.register(npc.getUID(), npc.getOwner());
 
 		player.sendMessage(StringUtils.wrap(npc.getName())
-				+ "'s armor was set to "
+				+ "'s brneni bylo nastaveno na "
 				+ StringUtils.wrap(MessageUtils.getMaterialName(mat.getId()))
 				+ ".");
 	}
@@ -473,7 +473,7 @@ public class BasicCommands {
 	@Command(
 			aliases = "npc",
 			usage = "tp",
-			desc = "teleport to an NPC",
+			desc = "teleport k NPC",
 			modifiers = "tp",
 			min = 1,
 			max = 1)
@@ -481,14 +481,14 @@ public class BasicCommands {
 	public static void teleportToNPC(CommandContext args, Player player,
 			HumanNPC npc) {
 		player.teleport(npc.getNPCData().getLocation());
-		player.sendMessage(ChatColor.GREEN + "Teleported you to "
-				+ StringUtils.wrap(npc.getStrippedName()) + ". Enjoy!");
+		player.sendMessage(ChatColor.GREEN + "Byl jsi teleportovan k "
+				+ StringUtils.wrap(npc.getStrippedName()) + ". Uzij si to!");
 	}
 
 	@Command(
 			aliases = "npc",
 			usage = "talkclose [true|false]",
-			desc = "set an NPC's talk-when-close setting",
+			desc = "nastavi aby NPC mluvilo kdyz se k nemu nekdo postavi blizko",
 			modifiers = "talkclose",
 			min = 2,
 			max = 2)
@@ -502,17 +502,17 @@ public class BasicCommands {
 		npc.getNPCData().setTalkClose(talk);
 		if (talk) {
 			player.sendMessage(StringUtils.wrap(npc.getStrippedName())
-					+ " will now talk to nearby players.");
+					+ " bude ted mluvit k hracum stojicim blizko.");
 		} else if (!talk) {
 			player.sendMessage(StringUtils.wrap(npc.getStrippedName())
-					+ " will stop talking to nearby players.");
+					+ " nebude uz mluvit k hracum stojicim blizkos.");
 		}
 	}
 
 	@Command(
 			aliases = "npc",
 			usage = "lookat [true|false]",
-			desc = "set an NPC's look-when-close setting",
+			desc = "nastavi aby se NPC divalo na hrace kdyz se k nemu nekdo blizko",
 			modifiers = "lookat",
 			min = 2,
 			max = 2)
@@ -526,10 +526,10 @@ public class BasicCommands {
 		npc.getNPCData().setLookClose(look);
 		if (look) {
 			player.sendMessage(StringUtils.wrap(npc.getStrippedName())
-					+ " will now look at players.");
+					+ " se odted bude divat na hrace.");
 		} else if (!look) {
 			player.sendMessage(StringUtils.wrap(npc.getStrippedName())
-					+ " will stop looking at players.");
+					+ " se odted nebude divat na hrace.");
 		}
 	}
 
@@ -537,21 +537,21 @@ public class BasicCommands {
 	@Command(
 			aliases = "npc",
 			usage = "id",
-			desc = "display an NPC's ID",
+			desc = "zobrazi ID NPC",
 			modifiers = "id",
 			min = 1,
 			max = 1)
 	@CommandPermissions("use.basic")
 	public static void displayNPCID(CommandContext args, Player player,
 			HumanNPC npc) {
-		player.sendMessage(ChatColor.GREEN + "The ID of this NPC is "
+		player.sendMessage(ChatColor.GREEN + "ID tohoto NPC je "
 				+ StringUtils.wrap("" + npc.getUID()) + ".");
 	}
 
 	@Command(
 			aliases = "npc",
 			usage = "select [id]",
-			desc = "select an NPC by its ID",
+			desc = "vybere NPC podle ID",
 			modifiers = "select",
 			min = 2,
 			max = 2)
@@ -561,9 +561,9 @@ public class BasicCommands {
 			HumanNPC npc) {
 		npc = NPCManager.get(Integer.valueOf(args.getString(1)));
 		if (npc == null) {
-			player.sendMessage(ChatColor.RED + "No NPC with the ID "
+			player.sendMessage(ChatColor.RED + "NPC s ID "
 					+ StringUtils.wrap(args.getString(1), ChatColor.RED)
-					+ " exists.");
+					+ " neexistuje.");
 		} else {
 			NPCManager.selectNPC(player, npc);
 			Messaging.send(player, npc, Constants.selectionMessage);
@@ -574,14 +574,14 @@ public class BasicCommands {
 	@Command(
 			aliases = "npc",
 			usage = "owner",
-			desc = "get the owner of an NPC",
+			desc = "zjisti vlastnika NPC",
 			modifiers = "owner",
 			min = 1,
 			max = 1)
 	@CommandPermissions("use.basic")
 	public static void getNPCOwner(CommandContext args, Player player,
 			HumanNPC npc) {
-		player.sendMessage(ChatColor.GREEN + "The owner of this NPC is "
+		player.sendMessage(ChatColor.GREEN + "Vlastnik tohoto NPC je "
 				+ StringUtils.wrap(npc.getOwner()) + ".");
 	}
 
@@ -589,7 +589,7 @@ public class BasicCommands {
 	@Command(
 			aliases = "npc",
 			usage = "setowner [name]",
-			desc = "set the owner of an NPC",
+			desc = "nastavi vlastnika NPC",
 			modifiers = "setowner",
 			min = 2,
 			max = 2)
@@ -597,8 +597,8 @@ public class BasicCommands {
 			HumanNPC npc) {
 		if (Permission.isAdmin(player)
 				|| Permission.canModify(player, npc, "basic")) {
-			player.sendMessage(ChatColor.GREEN + "The owner of "
-					+ StringUtils.wrap(npc.getStrippedName()) + " is now "
+			player.sendMessage(ChatColor.GREEN + "Vlastnik NPC "
+					+ StringUtils.wrap(npc.getStrippedName()) + " je ted "
 					+ StringUtils.wrap(args.getString(1)) + ".");
 			npc.getNPCData().setOwner(args.getString(1));
 		} else {
@@ -609,7 +609,7 @@ public class BasicCommands {
 	@Command(
 			aliases = "npc",
 			usage = "/npc [path|waypoints] (reset)",
-			desc = "toggle waypoint editing",
+			desc = "zapne editaci waypointu",
 			modifiers = { "path", "waypoints" },
 			min = 1,
 			max = 2)
@@ -621,7 +621,7 @@ public class BasicCommands {
 			int UID = npc.getUID();
 			if (editing == null) {
 				player.sendMessage(ChatColor.AQUA
-						+ StringUtils.listify("Waypoint Editing Controls"));
+						+ StringUtils.listify("Ovladani editace waypointu"));
 				player.sendMessage(StringUtils.wrap("Left")
 						+ " click adds a waypoint, while "
 						+ StringUtils.wrap("right") + " click acts as an undo.");
@@ -652,7 +652,7 @@ public class BasicCommands {
 	@Command(
 			aliases = "npc",
 			usage = "list (name) (page)",
-			desc = "view a list of NPCs for a player",
+			desc = "zobrazi seznam NPCs",
 			modifiers = "list",
 			min = 1,
 			max = 3)
@@ -674,7 +674,7 @@ public class BasicCommands {
 							"1");
 				} else {
 					player.sendMessage(ChatColor.RED
-							+ "Could not match player.");
+							+ "Nemuzu nalezt hrace.");
 				}
 			}
 			break;
@@ -684,7 +684,7 @@ public class BasicCommands {
 						ServerUtils.matchPlayer(args.getString(1)), npc,
 						args.getString(2));
 			} else {
-				player.sendMessage(ChatColor.RED + "Could not match player.");
+				player.sendMessage(ChatColor.RED + "Nemuzu nalezt hrace.");
 			}
 			break;
 		}
